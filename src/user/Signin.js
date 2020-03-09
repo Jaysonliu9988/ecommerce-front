12fb79a85b938ce5ab9680f3242a973bd11e95c9
Jaysonliu <leobest9688@gmail.com>
2020-03-09 20:46:28 +0800

    update user info

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -5,8 +5,8 @@ import { signin, authenticate, isAuthenticated } from "../auth";
 
 const Signin = () => {
     const [values, setValues] = useState({
-        email: "ryan@gmail.com",
-        password: "rrrrrr9",
+        email: "mike@gmail.com",
+        password: "mmmmmm9",
         error: "",
         loading: false,
         redirectToReferrer: false
